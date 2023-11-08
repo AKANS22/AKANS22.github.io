da3ae9fb5d973d1c28b6f112dfef8bfccc705855
Akans Udoka <Akansudoka7@gmail.com>
2023-11-08 11:55:13 +0100

    Completed education

--- Ezeji-Udoka-Akans/Controllers/EducationController.cs
+++ Ezeji-Udoka-Akans/Controllers/EducationController.cs
@@ -18,6 +18,25 @@ namespace Ezeji_Udoka_Akans.Controllers
                 Description = "Scrum Fundamentals Certified (SFC®) professionals are facilitators who ensure that the Scrum Team is provided with an environment conducive to completing the project successfully. The Scrum Master guides, facilitates, and teaches Scrum practices to everyone involved in the project; clears impediments for the team and ensures that Scrum processes are being followed."
 
             },
+
+             new EducationModel
+            {
+                SchoolName = "Imo State University",
+                Certificate = "B.Sc.INDUSTRIAL CHEMISTRY",
+                DateTime =     "2012 - 2016",
+
+                Description = ""
+
+            },
+
+              new EducationModel
+            {
+                SchoolName = "UNIQUE COMPUTER INSTITUTE OWERRI",
+                Certificate = "CERTIFICATE IN COMPUTER APPLICATION TO BUSINESS TRANSACTION",
+                DateTime =     "2012 - 2012 ",
+
+                Description = "Computer Application in Business is a course designed to train student on\r\nthe use of personal computer in the world of business. The knowledge gained in this course would lead to proficiency in electronic business management."
+            },
         };
 
 
